6bbacc53248a1590e7e6ef7ed8e5002b8d5053a7
Fin <fin.ambsdorf@gmail.com>
2019-01-19 23:05:37 -0500

    Added Raycast to  Image Capture

---
+++ LeapCorrect/Assets/LabelCast.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabelCast : MonoBehaviour {
+
+
+	public Transform Label;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+
+
+
+	}
+
+ 	void FixedUpdate()
+    {
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+		RaycastHit hit;
+
+        if (Physics.Raycast(this.transform.position, fwd, out hit))
+		{
+		print("I found something");
+;		 Instantiate(Label, hit.point, Quaternion.identity);	
+		}
+         
+    }
+
+
+}
